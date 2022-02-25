ff564c755f5e588b5b8db21e2d3820ac3b85762f
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-02-25 11:40:38 +0530

    #include<stdio.h> int main() {     int num,rev=0,temp;     scanf("%d",&num);     while(num!=0)     {         temp=num%10;         rev=rev*10+temp;         num=num/10;     }     printf("%d",rev); }

---
+++ Reverse_Integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num,rev=0,temp;
+    scanf("%d",&num);
+    while(num!=0)
+    {
+        temp=num%10;
+        rev=rev*10+temp;
+        num=num/10;
+    }
+    printf("%d",rev);
+}
