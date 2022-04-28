110208a7818038f14289a2820e0183533ef026cc
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-04-28 22:38:51 +0530

    #include<stdio.h> int main() {     int size,se,flag=0;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }     scanf(" %d",&se);     for(i=0;i<size;i++)     {         if(arr[i]==se)         {             flag=1;             break;         }     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int size,se,flag=0;
+    scanf("%d",&size);
+    int arr[size],i;
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("
+%d",&se);
+    for(i=0;i<size;i++)
+    {
+        if(arr[i]==se)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
