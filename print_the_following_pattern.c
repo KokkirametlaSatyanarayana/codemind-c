fcaafc18fbdf3636fb6913aec2751a51da482c9c
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-05-16 22:54:04 +0530

    #include<stdio.h> int main() {     int n,j,i;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=n;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,18 +3,11 @@ int main()
 {
     int n,j,i;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
     {
-        for(j=1;j<=n;j++)
+        for(j=n;j>=1;j--)
         {
-            if(i+j==n+1 || i==j)
-            {
-                printf("x");
-            }
-            else
-            {
-                printf("0");
-            }
+            printf("%d ",j);
         }
         printf("
 ");
