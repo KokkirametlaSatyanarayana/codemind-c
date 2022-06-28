eb51bb0a0acca4667493a082e61a23742104b40a
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-06-29 00:06:45 +0530

    #include<stdio.h> int main() {     int size,sum=0;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<size-1;i++)     {         if(arr[i-1]%2==0 && arr[i+1]%2==0)         {             if(arr[i]%2==0)             {                 sum++;             }         }     }     printf("%d",sum); }

--- count_even_numbers_between_even_numbers.c
+++ count_even_numbers_between_even_numbers.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int size,flag=0,sum=0,x;
+    int size,sum=0;
     scanf("%d",&size);
     int arr[size],i;
     for(i=0;i<size;i++)
     {
         scanf("%d",&arr[i]);
     }
-   for(i=0;i<size;i++)
-   {
-       if(arr[size-i-1]%2==0 && arr[size-i-3]%2==0)
-       {
-           sum=sum+1;
-       }
-   }
-   printf("%d",sum);
+    for(i=1;i<size-1;i++)
+    {
+        if(arr[i-1]%2==0 && arr[i+1]%2==0)
+        {
+            if(arr[i]%2==0)
+            {
+                sum++;
+            }
+        }
+    }
+    printf("%d",sum);
 }
