50ebeee07ba0e8aafa5172ee5c1ecf8ba77c7190
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2022-06-28 18:17:55 +0530

    #include<stdio.h> int main() {     int n,count=0,sum=0;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ ODD_Sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,count=0,sum=0;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
